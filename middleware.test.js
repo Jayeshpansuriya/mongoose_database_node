58170a34de106e5f051d8bd6874ab0341f4a4b7b
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-11 15:50:02 +0530

    mongoose middleware

---
+++ middleware.test.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+mongoose.set("debug", true)
+
+//?step 1 to connect to the database
+try {
+
+    await mongoose.connect("mongodb://localhost:27017/mongoose_middleware", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+} catch (error) {
+
+    console.error(error);
+    process.exit();
+}
+
+//?step 2 to create a schema
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true, min: 5, max: 120 },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+
+//we will use middleware 
+userSchema.pre(["updateOne","updateMany","findOneAndUpdate"],function(next){
+     this.set({ updatedAt: Date.now() });
+     next();
+})
+
+//?step 3 to create a model
+const Users = mongoose.model("User", userSchema);
+
+
+
+
+
+//step 4 to create a middleware
+// await Users.create({ name: "John Doe", email: "john@example.com", age: 100 });
+
+await Users.updateOne({ email: "john@example.com"},{$set:{age:100}});
+
+
+ 
+
+await mongoose.connection.close();
