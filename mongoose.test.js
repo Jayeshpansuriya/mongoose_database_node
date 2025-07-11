5d1511a889ea2e35920526e672b2f4b422727200
Jayesh Pansuriya <pansuriyajayesh786@gmail.com>
2025-07-11 15:03:31 +0530

    "creating data base withe mongoose in mongodb withw 3 step "

--- mongoose.test.js
+++ mongoose.test.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
+mongoose.set("debug", true)
+
 //?step 1 to connect to the database
 try {
 
     await mongoose.connect("mongodb://localhost:27017/mongoose_database", {
         useNewUrlParser: true,
-        useUnifiedTopology: true
-        mongoose.set("debug", true);
+        useUnifiedTopology: true,
     });
 
 } catch (error) {
@@ -14,3 +15,20 @@ try {
     console.error(error);
     process.exit();
 }
+
+//?step 2 to create a schema
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: true, min: 5, max: 120 },
+    createdAt: { type: Date, default: Date.now }
+});
+
+//?step 3 to create a model
+
+const Users = mongoose.model("User", userSchema);
+
+await Users.create({ name: "John Doe", email: "john@example.com", age: 30 });
+await mongoose.connection.close();
+
